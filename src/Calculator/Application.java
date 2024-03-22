34fbc082ef4827ab3ea31073d151b1c77d4a9641
Tanya Nair <Tanya.Nair@wisetechglobal.com>
2024-03-22 20:22:12 +0530

    remove unused import

--- src/Calculator/Application.java
+++ src/Calculator/Application.java
@@ -6,8 +6,6 @@ import Calculator.Service.Calculator;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
